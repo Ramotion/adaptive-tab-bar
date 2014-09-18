4d9a240f399beb8211d1074958bb0e665ef96558
Artem Kislitsyn <tork.the.knight@gmail.com>
2014-09-18 11:25:40 +0400

    clean trash

---
+++ AdaptiveTabBarExtension/AdaptiveTabBarExtension/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AdaptiveTabBarExtension
+//
+//  Created by Arcilite on 18.09.14.
+//  Copyright (c) 2014 Ramotion. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- AdaptiveTabBarExtensionSample/AdaptiveTabBarExtensionSampleTests/AdaptiveTabBarExtensionSampleTests.swift
+++ AdaptiveTabBarExtensionSample/AdaptiveTabBarExtensionSampleTests/AdaptiveTabBarExtensionSampleTests.swift
@@ -1,6 +1,6 @@
 //
-//  AdaptiveTabBarExtensionTests.swift
-//  AdaptiveTabBarExtensionTests
+//  AdaptiveTabBarExtensionSampleTests.swift
+//  AdaptiveTabBarExtensionSampleTests
 //
 //  Created by Arcilite on 18.09.14.
 //  Copyright (c) 2014 Ramotion. All rights reserved.
@@ -9,7 +9,7 @@
 import UIKit
 import XCTest
 
-class AdaptiveTabBarExtensionTests: XCTestCase {
+class AdaptiveTabBarExtensionSampleTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
