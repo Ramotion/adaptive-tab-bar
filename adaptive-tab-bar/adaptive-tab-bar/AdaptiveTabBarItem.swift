bd549e60698610c7560e2f507221687edb5f9031
Artem Kislitsyn <tork.the.knight@gmail.com>
2014-09-19 14:29:12 +0400

    implement tab bar item wraper

---
+++ adaptive-tab-bar/adaptive-tab-bar/AdaptiveTabBarItem.swift
@@ -0,0 +1,13 @@
+//
+//  AdaptiveTabBarItem.swift
+//  adaptive-tab-bar
+//
+//  Created by Arcilite on 19.09.14.
+//  Copyright (c) 2014 Ramotion. All rights reserved.
+//
+
+import UIKit
+
+class AdaptiveTabBarItem: UITabBarItem {
+   
+}
