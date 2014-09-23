abd234d981af4edee17237c4919ad45f87489759
Artem Kislitsyn <arcilite.kd@gmail.com>
2014-09-23 18:17:35 +0400

    clean code

--- adaptive-tab-bar/adaptive-tab-bar/TabBarViewController.swift
+++ adaptive-tab-bar/adaptive-tab-bar/TabBarViewController.swift
@@ -13,14 +13,6 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-       
-        
-          
-//        var frame:CGRect  = CGRectMake(0.0, 0, self.view.bounds.size.width, 48)
-//        var view = UIView(frame: frame);
-//        view.backgroundColor = UIColor(red: 0.0, green: 0, blue: 0, alpha: 0.0)
-//        self.tabBar.shadowImage = nil
-//        self.tabBar.insertSubview(view, atIndex: 0)
         
         if (UIDevice().systemVersion as NSString).floatValue >= 7.0{
             for  tbi in self.tabBar.items as [UITabBarItem] {
